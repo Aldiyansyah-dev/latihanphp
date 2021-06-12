b69897ec750170eb5503a083d678edff116b6b59
aldiyansyah <aldiyansyah.dev@gmail.com>
2021-06-12 20:26:54 +0700

    membuat koneksi dan tampil data

---
+++ detail.php
@@ -0,0 +1,34 @@
+<?php 
+require 'functions.php';
+
+//ambil id dari URL
+$id = $_GET['id'];
+
+//query mahasiswa berdasarkan id
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Mahasiswa</title>
+</head>
+<body>
+    <h2>Detail Mahasiswa</h2>
+    <ul>
+    <li><img src="image/<?= $mhs['gambar']; ?>" width="100" height="80"></li>
+    <li>NRP : <?= $mhs['nrp']; ?></li>
+    <li>Nama : <?= $mhs['nama']; ?></li>
+    <li>Email : <?= $mhs['email']; ?></li>
+    <li>Jurusan : <?= $mhs['jurusan']; ?></li>
+    <li><a href="">Update</a> | <a href="">Delete</a></li>
+    <li><a href="latihan3.php">Kembali ke Daftar Mahasiswa</a></li>
+
+
+    </ul>
+</body>
+</html>
---
+++ latihan3.php
@@ -0,0 +1,38 @@
+<?php 
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    <h2>Daftar Mahasiswa</h2>
+
+<table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+        <th>#</th>
+        <th>Gambar</th>
+        <th>Nama</th>
+        <th>Aksi</th>
+    </tr>
+
+    <?php $i = 1;
+     foreach($mahasiswa as $mhs) : ?>
+    <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="image/<?= $mhs['gambar']; ?>" width="100"  height="80"></td> 
+        <td><?= $mhs['nama']; ?></td>
+        <td>
+            <a href="detail.php?id=<?= $mhs['id']; ?>">Lihat Detail</a>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+</body>
+</html>
